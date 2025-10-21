3240087318d44bec0e0aee8fde1d05b857ac7cf2
MahmoudBinAbdulaziz <mahmoud.abdulazizz.1911@gmail.com>
2025-10-21 04:30:01 +0300

    typo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import "./app.css"
+import "./App.css"
 import { createTheme,ThemeProvider } from '@mui/material';
 import { TodoContext } from './context/todoContext.js';
 const theme = createTheme({
